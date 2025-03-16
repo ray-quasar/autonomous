c316a0a9b800e9e1830fcd703545088ad41d9c3a
VincentMig <VMIGLIACCIO@UCSB.edu>
2025-03-16 14:21:03 -0700

    follow_gap.py

--- follow_gap/follow_gap/follow_gap.py
+++ follow_gap/follow_gap/follow_gap.py
@@ -14,8 +14,8 @@ class FollowGapDisparity(Node):
         
         # Parameters
         self.safe_distance = 1.0         # Mask out obstacles closer than 3 m.
-        self.disparity_threshold = 0.33   # Disparity threshold (meters)
-        self.bias_factor = 0.1           # Fraction of (center - midpoint) to shift the target
+        self.disparity_threshold = 0.6   # Disparity threshold (meters)
+        self.bias_factor = 0.0           # Fraction of (center - midpoint) to shift the target
         
         # Drive parameters
         self.base_speed = 0.75           # Base speed (m/s) when turning mildly
