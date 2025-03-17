590d2c6efc6dabe53b8d1e439dcbeed107d69b11
Sanjot <sanjotbains@gmail.com>
2025-03-17 13:19:06 -0700

    more test files

--- follow_gap/follow_gap/scratch_sheet.py
+++ follow_gap/follow_gap/scratch_sheet.py
@@ -1,15 +1,23 @@
 import numpy as np
-from scan_data import laser_scan
+import math
+from test_scan import laser_scan
 
 # This is a toy example of the disparityExtender algorithm
 # It takes in a LiDAR scan and outputs a steering angle and speed
 # The LiDAR scan is like a LaserScan message and is defined as an array in scan_data.py
 
 # First, we do preprocessing on the raw scan data
+# We copy in the raw scan data into a NumPy array
 valid_ranges = np.copy(laser_scan["ranges"])
+# Let's check that the copy worked by printing the first 5 values
+# print(max(valid_ranges))
+# print(min(valid_ranges))
+print(valid_ranges[:5])
 
 # We know we cannot trust values that are beyond the specified range_min and range_max
 valid_ranges = np.clip(valid_ranges, laser_scan["range_min"], laser_scan["range_max"])
+# print(max(valid_ranges))
+# print(min(valid_ranges))
 
 # F1T recommends clipping to even lower than range_max to prevent swinging in long straightaways
 # But this might not be necessary for a track with no right angles
@@ -26,27 +34,27 @@ valid_ranges = np.nan_to_num(valid_ranges)
 # We assume that theta = 0 is directly in front of the car
 # We want to ignore the rear 90 degrees of the scan
 # This bit of code assumes that the scan is 360 degrees and keeps the middle half
-valid_ranges = valid_ranges[len(valid_ranges)//4:3*len(valid_ranges)//4]
+# valid_ranges = valid_ranges[len(valid_ranges)//4:3*len(valid_ranges)//4]
 # 90 degrees might not be enough, but this can be parameterized 
 
 # Next we need a mechanism to identify disparities in the scan
 # A disparity is a large jump in range values
 # We expect the car to running in a track about 2 meters wide
 # So we can set a guesstimate for disparities being at least 0.5 meters jump in range values
-disparity_check = 0.5
+# disparity_check = 0.5
 # I think this should actually be something about the size and turning radius of the car but I have no idea how to calculate that
 
 # BUILD A FUNCTTION TO DETECT DISPARITIES
 
-def find_disparities(ranges, check_value):
+# def find_disparities(ranges, check_value):
 
 
-    return # return the indices of the disparities
+#     return # return the indices of the disparities
 
 # Now that we know where the disparities are, we can EXTEND them
 # For each disparity, we want to extend it towards the increase in range values
 # The extension distance needs to be half the width of the car
-extension_distance = 0.5
+# extension_distance = 0.5
 # But the amount of lidar scan points we rewrite depends on the distance from the car
 # The further away the disparity, the fewer points we need to rewrite
 # To calculate the number of points to rewrite, we can use the angle increment of the scan
@@ -57,9 +65,9 @@ extension_distance = 0.5
 
 # BUILD A FUNCTION TO EXTEND DISPARITIES
 
-def extend_disparities(ranges, disparities, extension_distance):
+# def extend_disparities(ranges, disparities, extension_distance):
 
-    return # return the extended ranges
+#     return # return the extended ranges
 
 # Now that we have extended the disparities, we have a set of virtual ranges in configuration space
 # There are two options/strategies for following the gap
@@ -72,15 +80,15 @@ def extend_disparities(ranges, disparities, extension_distance):
 
 # BUILD A FUNCTION TO FIND THE LARGEST GAP
 
-def find_largest_gap(ranges):
+# def find_largest_gap(ranges):
 
-    return # return the start and end indices of the largest
+#     return # return the start and end indices of the largest
 
 # BUILD A FUNCTION TO FIND THE DEEPEST
 
-def find_deepest_gap(ranges):
+# def find_deepest_gap(ranges):
     
-    return # return the start and end indices of the deepest
+#     return # return the start and end indices of the deepest
 
 # Now that we have the desired gap, we can calculate the steering angle and speed
 
